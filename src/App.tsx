f9d3a98d0e0a6ab55edb74bff189c86d8b101164
unknown <jcarsan1000@gmail.com>
2022-03-09 18:14:07 -0300

    Criação do projeto em React, criação e estilização do componente Header

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//import './App.css';
+import styled from 'styled-components';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+
+function App() {
+  return (
+    <>
+      <Header/> 
+      <GlobalStyle/> 
+    </>
+  );
+}
+
+export default App;
