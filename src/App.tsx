d9ee57fb412d16cfce208f93257f6ee082451b02
unknown <jcarsan1000@gmail.com>
2022-03-12 10:41:09 -0300

    Criação do componente modal da aplicação usando a biblioteca react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import './App.css';
 import styled from 'styled-components';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { TransactionsTable } from './components/TransactionsTable/TransactionsTable';
 import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 function App() {
+  
+  const [isNewTransactionModalOpen,setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header/> 
+      <Header onOpenNewTransactionModal={ handleOpenNewTransactionModal } /> 
       <Dashboard/>
+      <Modal 
+            isOpen={ isNewTransactionModalOpen } 
+            onRequestClose={ handleCloseNewTransactionModal }>
+        <h2>Cadastrar Transação</h2>
+      </Modal>
       <GlobalStyle/> 
     </>
   );
